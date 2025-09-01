4e0c3114146e9e4df0f9d8c7d9a8c3a6382145a8
Codexophile <jaliya.chirantha@gmail.com>
2025-09-01 16:25:20 +0530

    uww-net-main.py: clean terminal at startup and enable wallpaper data printing

--- uww-net-main.py
+++ uww-net-main.py
@@ -6,10 +6,12 @@ from wallpaper_scraper import get_one_wallpaper_after_shuffle
 # To run the program:
 if __name__ == "__main__":
     # 0. Monitor information at startup
+    # clean terminal
+    os.system("cls" if os.name == "nt" else "clear")
     monitors_list = gather_monitors()
     data = get_one_wallpaper_after_shuffle()
     if data:
         print("\nSuccessfully extracted data for one wallpaper after shuffle:")
-        # print(data)
+        print(data)
     else:
         print("\nFailed to extract wallpaper data.")
